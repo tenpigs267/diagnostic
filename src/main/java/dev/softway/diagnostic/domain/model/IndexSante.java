cc7cd0b3a27989ecd02858a2867ec159150d558b
tenpigs267 <126336487+tenpigs267@users.noreply.github.com>
2025-05-19 16:24:34 +0200

    ajout d'un TODO pour gestion ultérieurs des cas limites

--- src/main/java/dev/softway/diagnostic/domain/model/IndexSante.java
+++ src/main/java/dev/softway/diagnostic/domain/model/IndexSante.java
@@ -2,6 +2,8 @@ package dev.softway.diagnostic.domain.model;
 
 public record IndexSante(int valeur) {
 
+    //TODO : voir si besoin de, et comment, traiter les valeurs 0 ou négatives avec le PO
+
     public boolean indiqueProblemeCardiaque() {
         return (valeur % 3) == 0;
     }
