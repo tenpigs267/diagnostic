4dfe1763b99c66bc16810e695fcc629145c98660
tenpigs267 <126336487+tenpigs267@users.noreply.github.com>
2025-05-19 16:11:10 +0200

    probleme cardiaque : ajout test négatif + ParameterizedTest

--- src/main/java/dev/softway/diagnostic/domain/model/IndexSante.java
+++ src/main/java/dev/softway/diagnostic/domain/model/IndexSante.java
@@ -3,6 +3,6 @@ package dev.softway.diagnostic.domain.model;
 public record IndexSante(int valeur) {
 
     public boolean problemeCardiaque() {
-        return true;
+        return (valeur % 3) == 0;
     }
 }
--- src/test/java/dev/softway/diagnostic/domain/model/IndexSanteTest.java
+++ src/test/java/dev/softway/diagnostic/domain/model/IndexSanteTest.java
@@ -1,17 +1,30 @@
 package dev.softway.diagnostic.domain.model;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class IndexSanteTest {
 
-    @Test
-    void devrait_identifier_probleme_cardiaque(){
+    @ParameterizedTest
+    @ValueSource(ints = {0, 3, 6, 9, 15})
+    void devrait_identifier_probleme_cardiaque(int valeurIndexSante){
         //Given
-        IndexSante indexSante = new IndexSante(3);
+        IndexSante indexSante = new IndexSante(valeurIndexSante);
 
         //When Then
         assertTrue(indexSante.problemeCardiaque());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 4, 5, 7, 8, 10})
+    void ne_devrait_pas_identifier_probleme_cardiaque(int valeurIndexSante){
+        //Given
+        IndexSante indexSante = new IndexSante(valeurIndexSante);
+
+        //When Then
+        assertFalse(indexSante.problemeCardiaque());
     }
 }
